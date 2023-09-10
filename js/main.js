547ec47527ab68370b981cf52e2b2fcc889390c4
tokkaya79 <tokkaya79@gmail.com>
2023-09-10 17:21:06 +0200

    added section certificates with adaptiv

--- js/main.js
+++ js/main.js
@@ -4,3 +4,31 @@ const menuMobile = document.querySelector('.header-menu__list');
 menuBtn.addEventListener('click', () => {
     menuMobile.classList.toggle('menu--open');
 });
+
+const swiperOne = new Swiper('.rewiews__slider', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+const swiperTwo = new Swiper('.certificates__slider', {
+    loop: true,
+    slidesPerView: 2.3,
+    spaceBetween: 20,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    // breakpoints: {
+    //     480: {
+    //         slidesPerView: 2,
+    //         
+    //     },
+    //     360: {
+    //         slidesPerView: 1,
+    //     }
+    // }
+});
+
+swiperTwo.width = auto;
